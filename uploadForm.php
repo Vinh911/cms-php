2db14ed27d6186287c2e447b29939281c1af4487
Vinh <nguyenvinh1997@yahoo.de>
2022-07-21 17:25:34 +0200

    enctype to upload multiple images

--- uploadForm.php
+++ uploadForm.php
@@ -18,14 +18,14 @@ echo "Hallo User: ".$userid;
 </head>
 
 <body>
-    <form action="upload.php" method="post">
+    <form action="upload.php" method="post" enctype="multipart/form-data">
         <label for="title">Titel:</label><br>
         <input type="text" id="title" name="title" required><br>
         <label for="description">Beschreibung:</label><br>
         <input type="text" id="description" name="description" required><br>
         <label for="text">Text:</label><br>
         <textarea id="text" name="text" required></textarea><br>
-        <label for="images">Bild:</label><br>
+        <label for="images">Bilder:</label><br>
         <input type="file" id="images" name="images" multiple required><br>
         <input type="submit" name="submit" value="Submit">
     </form>
