8e4f9fe3c36e7cc541a0632807502f1ddbb79592
Vinh <nguyenvinh1997@yahoo.de>
2022-07-24 01:43:50 +0200

    fix: absolute path

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="styles.css">
 <ul class="navbar">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="uploadForm.php">Upload</a></li>
-    <li><a href="edit.php">Bearbeiten</a></li>
-    <li style="float:right"><a href="api/logout.php">Logout</a></li>
+    <li><a href="/index.php">Home</a></li>
+    <li><a href="/uploadForm.php">Upload</a></li>
+    <li><a href="/edit.php">Bearbeiten</a></li>
+    <li style="float:right"><a href="/api/logout.php">Logout</a></li>
 </ul>
